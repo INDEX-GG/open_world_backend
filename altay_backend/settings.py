f040cbec29a8b2c67604f92ead79ff2a49994d41
Gosha Kokorin <go_2002@mail.ru>
2022-10-31 10:31:52 +0500

    add static

--- altay_backend/settings.py
+++ altay_backend/settings.py
@@ -71,23 +71,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'altay_backend.wsgi.application'
 
 # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config.NAME,
-#         'USER': config.USER,
-#         'PASSWORD': config.PASSWORD,
-#         'HOST': config.HOST,
-#         'PORT': config.PORT
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.NAME,
+        'USER': config.USER,
+        'PASSWORD': config.PASSWORD,
+        'HOST': config.HOST,
+        'PORT': config.PORT
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
