ac8ed51ec4b6ed41e0e8be167e2bec146ea8e7a6
Gosha Kokorin <go_2002@mail.ru>
2022-11-03 10:50:35 +0500

    change sites

--- altay_backend/settings.py
+++ altay_backend/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
 
     'drf_yasg',
     'rest_framework',
