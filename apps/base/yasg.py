4dd2993c638355d9d847598dc0c65977fd121236
Gosha Kokorin <go_2002@mail.ru>
2022-11-03 09:40:22 +0500

    change swagger

--- apps/base/yasg.py
+++ apps/base/yasg.py
@@ -1,5 +1,4 @@
-from django.urls import path, re_path
-from rest_framework import permissions
+from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -10,11 +9,10 @@ schema_view = get_schema_view(
         description="Docs for api Altay Backend",
     ),
     public=True,
-    # permission_classes=(permissions.AllowAny,),
+    url='https://testguru.ru/'
 )
 
 urlpatterns = [
-    # path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/v1/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
